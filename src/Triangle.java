e9b1c2f2462b02edc86eddacbdc66455ac6603d7
josemartinthomas14 <josemartinthomas14@example.com>
2026-04-06 20:43:29 +0530

    Solve experiment 3

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,7 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* * *");
+        System.out.println("* * * * *");
+    }
 }
